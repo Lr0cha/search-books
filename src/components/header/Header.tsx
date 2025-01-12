5f85809deef3d9524500791b3d1b3f21d71a6aa2
Lucas Rocha <ldrocha16@gmail.com>
2025-01-12 19:07:25 -0300

    Adicionando componente header as pagínas com router

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h2>Header da pág</h2>
+    </header>
+  );
+};
+
+export default Header;
--- src/router.tsx
+++ src/router.tsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Detail from "./pages/detail/Detail";
 import NotFound from "./pages/notFound/NotFound";
+import Layout from "./components/layout/Layout";
 
 // criar rotas de navegação
 const router = createBrowserRouter([
   {
+    element: <Layout />, // renderiza o header
     children: [
       {
         path: "/",
