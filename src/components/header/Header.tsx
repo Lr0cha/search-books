eaadbb30d909747b70674826a3150e3e901227fb
Lucas Rocha <ldrocha16@gmail.com>
2025-02-06 15:47:57 -0300

    Estilização do header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,11 @@
 const Header = () => {
   return (
-    <header>
-      <h2>Header da pág</h2>
+    <header className="bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 p-6">
+      <div className="flex items-center justify-center">
+        <h1 className="text-4xl font-semibold text-white text-center">
+          Search <span className="text-blue-200">Books</span>
+        </h1>
+      </div>
     </header>
   );
 };
